bc8ddfb95814dc21198e02cb851982c025104507
Manuel Echavarria <mechavarria@aldereca.com>
2023-03-15 16:59:02 -0400

    first commit

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Http\Resources\BookResource;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return BookResource::collection(Book::all());
+    }
+
+    public function store(StoreBookRequest $request)
+    {
+        $book = Book::create($request->validated());
+
+        return BookResource::make($book);
+    }
+
+    public function show(Book $book)
+    {
+        return BookResource::make($book);
+    }
+
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        $book->update($request->validated());
+
+        return BookResource::make($book);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Controllers/Api/ChapterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Chapter;
+use App\Http\Requests\StoreChapterRequest;
+use App\Http\Requests\UpdateChapterRequest;
+use App\Http\Resources\ChapterResource;
+
+class ChapterController extends Controller
+{
+    public function index()
+    {
+        return ChapterResource::collection(Chapter::paginate(2));
+    }
+
+    public function store(StoreChapterRequest $request)
+    {
+        $chapter = Chapter::create($request->validated());
+
+        return ChapterResource::make($chapter);
+    }
+
+    public function show(Chapter $chapter)
+    {
+        return ChapterResource::make($chapter);
+    }
+
+    public function update(UpdateChapterRequest $request, Chapter $chapter)
+    {
+        $chapter->update($request->validated());
+
+        return ChapterResource::make($chapter);
+    }
+
+    public function destroy(Chapter $chapter)
+    {
+        $chapter->delete();
+
+        return response()->noContent();
+    }
+}
